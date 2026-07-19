459280fb4b1a09639f8c1978732b41840af76c4a
rager306 <248269686+rager306@users.noreply.github.com>
2026-07-19 22:21:26 +0000

    Fix remaining CI linter failures

--- api/embed/coalescedembedder_test.go
+++ api/embed/coalescedembedder_test.go
@@ -39,7 +39,7 @@ func TestCoalescingEmbedderBurst(t *testing.T) {
 	start := make(chan struct{})
 
 	for i := 0; i < N; i++ {
-		go func(i int) {
+		go func(i int) { //nolint:unparam // intentionally capturing loop variable
 			defer wg.Done()
 			<-start
 			texts := []string{
--- api/handlers/embeddings.go
+++ api/handlers/embeddings.go
@@ -260,7 +260,7 @@ func buildEmbeddingsResponse(embeddings [][]float32, dims int, encodingFormat, m
 	}
 
 	return embed.EmbeddingsResponse{
-		Object: "list",
+		Object: "list", //nolint:goconst // literal "list" is an OpenAI API schema constant
 		Data:   data,
 		Model:  modelID,
 		Usage: embed.Usage{
